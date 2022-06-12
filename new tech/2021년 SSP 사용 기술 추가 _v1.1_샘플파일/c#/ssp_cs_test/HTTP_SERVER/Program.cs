826ec19bfa65ecc177293f1e70fe818270563b6f
limilwoo <limilwoo@gmail.com>
2022-06-13 01:02:15 +0900

    [최초커밋]

---
+++ new tech/2021년 SSP 사용 기술 추가 _v1.1_샘플파일/c#/ssp_cs_test/HTTP_SERVER/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace SP_TEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add("http://127.0.0.1:8080/");
+            listener.Start();
+
+            while(true)
+            {
+                var context = listener.GetContext();
+                Console.WriteLine("Request : " + context.Request.Url);
+                byte[] data = Encoding.UTF8.GetBytes("HelloWorld");
+                context.Response.OutputStream.Write(data, 0, data.Length);
+                context.Response.StatusCode = 200;
+                context.Response.Close();
+            }
+        }
+    }
+}
---
+++ new tech/2021년 SSP 사용 기술 추가 _v1.1_샘플파일/c#/ssp_cs_test/JSON/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace SP_TEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JObject json = new JObject();
+            json["name"] = "John Doe";
+            json["salary"] = 300100;
+            string jsonstr = json.ToString();
+            Console.WriteLine("Json : " + jsonstr);
+            JObject json2 = JObject.Parse(jsonstr);
+            Console.WriteLine($"Name : {json2["name"]}, Salary : {json2["salary"]}");
+        }
+    }
+}
