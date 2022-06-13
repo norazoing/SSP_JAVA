4b07839fae5a962deea44657cf26b9420e228080
norazoing <limilwoo@gmail.com>
2022-06-13 16:11:41 +0900

    Update JsonSolve2.java

--- JSON/JAVA/JsonSolve2.java
+++ JSON/JAVA/JsonSolve2.java
@@ -15,10 +15,11 @@ public class JsonSolve2 {
 			JsonReader reader = new JsonReader(new FileReader(filePath));
 			JsonObject jsonObj = gson.fromJson(reader, JsonObject.class);
 
-			String name = jsonObj.get("name").getAsString();
-			int age = jsonObj.get("age").getAsInt();
+			String name = jsonObj.get("name").getAsString(); // 타입을 아는 경우
+			int age = jsonObj.get("age").getAsInt(); // 타입을 아는 경우만 사용
 			System.out.println("name:"+name + "("+age+")");
 			
+		        // Json Array선언해서 get(1)은 두번째 배열에 있는 name과 age를 가져와서 출력 c언어 배열인덱스처럼
 			JsonArray jsonArr = jsonObj.get("children").getAsJsonArray();
 			JsonObject jsonObj2 = jsonArr.get(1).getAsJsonObject();
 			name = jsonObj2.get("name").getAsString();
@@ -30,3 +31,11 @@ public class JsonSolve2 {
 		}
 	}
 }
+
+/*{"name":"spiderman",
+ * "age":45,"married":true,
+ * "specialty":["martial art","gun"],
+ * "vaccine":{"1st":"done","2nd":"expected","3rd":null},
+ * "children":[{"name":"spiderboy","age":10},{"name":"spidergirl","age":8}],
+ * "address":null}
+ */
